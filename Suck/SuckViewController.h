f02420d3752707f6deafbbcd27d445d205e6d7bf
Christian Sousa <neocsr@gmail.com>
2011-08-22 10:10:05 -0500

    Initial commit

--- Suck/SuckViewController.h
+++ Suck/SuckViewController.h
@@ -10,6 +10,20 @@
 
 @interface SuckViewController : UIViewController {
     
+    UIView *testView;
+    UILabel *duration;
+    UISlider *slider;
+
+    UIView *container;
 }
+
+@property (nonatomic, retain) IBOutlet UIView *container;
+@property (nonatomic, retain) IBOutlet UIView *testView;
+@property (nonatomic, retain) IBOutlet UILabel *duration;
+@property (nonatomic, retain) IBOutlet UISlider *slider;
+
+- (void)suckView:(UITapGestureRecognizer *)recognizer;
+- (IBAction)resetView:(id)sender;
+- (IBAction)updateLabel:(id)sender;
 
 @end
--- Suck/SuckViewController.m
+++ Suck/SuckViewController.m
@@ -9,9 +9,17 @@
 #import "SuckViewController.h"
 
 @implementation SuckViewController
+@synthesize container;
+@synthesize testView;
+@synthesize duration;
+@synthesize slider;
 
 - (void)dealloc
 {
+    [testView release];
+    [duration release];
+    [slider release];
+    [container release];
     [super dealloc];
 }
 
@@ -25,17 +33,38 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // Attach tap detector
+    UITapGestureRecognizer *singleFingerTap = 
+    [[UITapGestureRecognizer alloc] initWithTarget:self 
+                                            action:@selector(suckView:)];
+    singleFingerTap.cancelsTouchesInView = NO;
+    [container addGestureRecognizer:singleFingerTap];
+    [singleFingerTap release];
+    
+    // Setup test image
+    UIImage *testImage = [UIImage imageNamed:@"squares_transparent_200x200.png"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:testImage];
+    [testView addSubview:imageView];
+    [imageView release];
+    
+    
+
 }
-*/
 
 - (void)viewDidUnload
 {
+    [self setTestView:nil];
+    [self setDuration:nil];
+    [self setSlider:nil];
+    [self setContainer:nil];
     [super viewDidUnload];
+    
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -44,6 +73,32 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)suckView:(UITapGestureRecognizer *)recognizer {
+    NSLog(@"%s", __FUNCTION__);
+    
+    CGPoint location = [recognizer locationInView:[recognizer.view superview]];
+    NSLog(@"Point (%.2f, %.2f)", location.x, location.y);
+
+    [testView setAlpha:0];
+    
+    [UIView beginAnimations:@"suck" context:NULL];
+    [UIView setAnimationTransition:103 forView:testView cache:YES];
+    [UIView setAnimationDuration:round(self.slider.value)];
+    [UIView setAnimationPosition:location];
+    [UIView commitAnimations];
+    
+}
+
+- (IBAction)resetView:(id)sender {
+    NSLog(@"%s", __FUNCTION__);
+    
+    [testView setAlpha:1];
+}
+
+- (IBAction)updateLabel:(id)sender {
+    [self.duration setText:[NSString stringWithFormat:@"%.0fs", round(self.slider.value)]];
 }
 
 @end
